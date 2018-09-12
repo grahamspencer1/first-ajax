3bd2ecf1f5ddc73270c81180f6fbc5100bb7eddc
Graham Spencer <grahamspence@gmail.com>
2018-09-12 15:46:47 -0400

    step 7 complete

--- ajax.js
+++ ajax.js
@@ -2,14 +2,20 @@ document.addEventListener("DOMContentLoaded", function() {
 
   var step12Button = document.querySelector('#step12 button');
   var step3456Button = document.querySelector('#step3456 button');
+  var step7Button = document.querySelector('#step7 button');
+  var step8Button = document.querySelector('#step8 button');
+  var step9Button = document.querySelector('#step9 button');
+
+  var section12 = document.querySelector('#step12');
   var section3456 = document.querySelector('#step3456');
+  var section7 = document.querySelector('#step7');
+  var section8 = document.querySelector('#step8');
+  var section9 = document.querySelector('#step9');
 
   step12Button.addEventListener('click', function() {
     $.ajax({
       url: 'http://first-ajax-api.herokuapp.com/',
       method: 'GET',
-      // data: ______,
-      // dataType: ______
     }).done(function (responseData) {
       console.log("Hello world!");
     });
@@ -19,8 +25,6 @@ document.addEventListener("DOMContentLoaded", function() {
     $.ajax({
       url: 'http://first-ajax-api.herokuapp.com/ping',
       method: 'GET',
-      // data: ______,
-      // dataType: ______
     }).done(function (responseData) {
       console.log(responseData);
       var paragraph = document.createElement('p');
@@ -34,6 +38,18 @@ document.addEventListener("DOMContentLoaded", function() {
     }).always(function () {
       console.log('Request finished! Horray!')
     });
+  });
+
+  step7Button.addEventListener('click', function(){
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/count',
+      method: 'GET',
+    }).done(function (responseData) {
+      console.log(responseData);
+      var counter = document.createElement('p');
+      counter.innerText = (responseData);
+      section7.append(counter);
+    })
   });
 
 });
